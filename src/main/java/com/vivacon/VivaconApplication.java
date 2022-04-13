cf6355637a4df35e7601c450da5467f475b21844
hungdoan-tech <hungdoan426@gmail.com>
2022-04-13 22:52:38 +0700

    Change the Spring Boot JVM and DB timezone

--- src/main/java/com/vivacon/VivaconApplication.java
+++ src/main/java/com/vivacon/VivaconApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableTransactionManagement
 @SpringBootApplication
 public class VivaconApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VivaconApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTheDefaultTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
