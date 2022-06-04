948ef5a616ff2b653f8da84c7f3c849fad282c79
hungdoan-tech <hungdoan426@gmail.com>
2022-06-04 17:26:04 +0700

    Add method for fetching Settings in SettingRepository

--- src/main/java/com/vivacon/controller/UserSettingController.java
+++ src/main/java/com/vivacon/controller/UserSettingController.java
@@ -1,21 +1,28 @@
 package com.vivacon.controller;
 
+import com.vivacon.dto.response.SettingResponse;
 import com.vivacon.entity.enum_type.SettingType;
 import com.vivacon.service.SettingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserSettingController {
 
     private SettingService settingService;
 
-    public UserSettingController() {
+    public UserSettingController(SettingService settingService) {
 
     }
 
     public ResponseEntity<Object> changeSetting(SettingType settingType, String value) {
         settingService.changeSetting(settingType, value);
         return ResponseEntity.ok(null);
+    }
+
+    public List<SettingResponse> getSettings() {
+        return settingService.getSettings();
     }
 }
--- src/main/java/com/vivacon/service/SettingService.java
+++ src/main/java/com/vivacon/service/SettingService.java
@@ -1,10 +1,15 @@
 package com.vivacon.service;
 
+import com.vivacon.dto.response.SettingResponse;
 import com.vivacon.entity.enum_type.SettingType;
+
+import java.util.List;
 
 public interface SettingService {
 
     void changeSetting(SettingType settingType, String value);
 
     boolean evaluateSetting(SettingType settingType, String settingValue, String compareValue);
+
+    List<SettingResponse> getSettings();
 }
